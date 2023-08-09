525f0002f72bc431c4d14cf6be8e6e221bd696be
Praneeth Kolichala <kolichp@amazon.com>
2023-08-09 15:12:14 -0400

    Rename files

--- tinytodo/present_tinytodo.py
+++ tinytodo/present_tinytodo.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         """toggle_task('List::"l0"', 1)  # Emina also has permission to update the list""",
         """get_list('List::"l0"')""",
         """stop_server()""",
-        """start_server('./huge_entities.json')""",
+        """start_server('./entities.huge.json')""",
         """set_user(User('b02cc63c-9daf-465b-82d9-bff7e113a6d9'))""",
         """get_list('List::"2fe39ac9-c04c-424e-9f70-7083de89e51a"')""",
         """get_lists()""",
